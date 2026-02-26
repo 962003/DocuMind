103cf1456a34cab8cd4a0675b910fa366b128bd6
swarda.jangam <swarda.jangam@humancloud.co.in>
2026-02-26 10:36:22 +0530

    file upload faster

--- frontend/src/lib/config.ts
+++
@@ -1,7 +0,0 @@
-const DEFAULT_BACKEND_URL = "http://127.0.0.1:8000";
-const DEFAULT_FRONTEND_URL = "http://127.0.0.1:3000";
-
-export const appConfig = {
-  backendUrl: process.env.NEXT_PUBLIC_BACKEND_URL || DEFAULT_BACKEND_URL,
-  frontendUrl: process.env.NEXT_PUBLIC_FRONTEND_URL || DEFAULT_FRONTEND_URL,
-};
